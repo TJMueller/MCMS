978b2bd5e8dea8b691596c08ca0d546e1f5a1411
garciajg <pepe_garcia1606@yahoo.com>
2015-03-17 15:36:58 -0500

    text fields there

--- MCMS2/CreatureViewController.m
+++ MCMS2/CreatureViewController.m
@@ -9,6 +9,8 @@
 #import "CreatureViewController.h"
 
 @interface CreatureViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameCVCTextField;
+@property (weak, nonatomic) IBOutlet UITextField *detailCVCTextField;
 
 @end
 
@@ -16,20 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.nameCVCTextField.text = self.creature.name;
+    self.detailCVCTextField.text = self.creature.details;
 
 }
 
-On edit button
+//On edit button
 -(IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
 
     //sets the editing capability on or off depending on button's title
     //animates going both ways switching between the two
     if ([sender.title isEqualToString:@"Edit"]) {
         sender.title = @"Done";
-        [self.rootTableView setEditing:YES animated:YES];
+//        [self.rootTableView setEditing:YES animated:YES];
     } else{
         sender.title = @"Edit";
-        [self.rootTableView setEditing:NO animated:YES];
+//        [self.rootTableView setEditing:NO animated:YES];
     }
 }
 
